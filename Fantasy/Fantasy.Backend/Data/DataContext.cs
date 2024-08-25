78c4540b8a91539e5da79f45b744105b4bdeaca7
LIZET VANESA MEDINA CUADRADO <vanesitamedina@gmail.com>
2024-08-25 00:17:56 -0500

    Add countries entity

---
+++ Fantasy/Fantasy.Backend/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Fantasy.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fantasy.Backend.Data;
+
+public class DataContext : DbContext
+{
+    //constructor
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {        
+    }
+
+    //cada tabla es una propiedad
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //tabla country vas a tener un indice por nombre y vas a ser un indice unico
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+    }
+}
